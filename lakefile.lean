c195e785dca6c452c22b766e8d0445d8dbfd44ca
Csharpython <ykt8910@gmail.com>
2024-02-29 17:55:20 +0900

    Mathlibが不要だったのでrequire mathlibを削除

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,6 @@
 import Lake
 open System Lake DSL
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "36892106064555c85a0bbf805c5520a0b765b7b9"
-
 package theorems
 @[default_target]
 lean_lib «Myfile» where
