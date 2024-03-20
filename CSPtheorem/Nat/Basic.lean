61ca4647cc1c9b5553ee48d03f1f441b81a395d9
Csharpython <ykt8910@gmail.com>
2024-03-20 18:12:34 +0900

    思いついた定理を適当に追加

--- CSPtheorem/Nat/Basic.lean
+++ CSPtheorem/Nat/Basic.lean
@@ -2,7 +2,6 @@ import Lean
 namespace Nat
 
 theorem lt_eq_succ_le {a b:Nat}: a < b ↔ succ a ≤ b := ⟨λt↦t,λt↦t⟩ -- 草
-
 theorem exist_add_of_le {a b:Nat}(h:b≤a): ∃(n:Nat),a=b+n := by
   induction h
   case refl => exists 0
@@ -12,3 +11,13 @@ theorem exist_add_of_le {a b:Nat}(h:b≤a): ∃(n:Nat),a=b+n := by
       exists w+1
 
 theorem one_add {a:Nat} : 1 + a = succ a := by simp[Nat.add_comm]
+
+@[simp] theorem pow_one {a:Nat} : a ^ 1 = a := by rw[show 1 = Nat.succ 0 from rfl,pow_succ,pow_zero,Nat.one_mul]
+@[simp] theorem one_pow {a:Nat} : 1 ^ a = 1 := by
+  induction a
+  case zero => rfl
+  case succ n dn => simp[pow_succ,dn]
+@[simp] theorem zero_pow {a:Nat}(h:a≠0) : 0 ^ a = 0 := by
+  cases a
+  case zero => contradiction
+  case succ => simp[pow_succ]
